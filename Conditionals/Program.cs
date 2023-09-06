27d700dfc34a06b967c77b5200c9353512e914dd
Arktoss <111780704+Arktoss@users.noreply.github.com>
2023-09-06 12:13:49 -0400

    Add project files.

---
+++ Conditionals/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IfElseStuff();
+
+            Console.ReadLine();
+        }
+
+        static void IfElseStuff()
+        {
+            Console.WriteLine("Please enter how much gold you have: ");
+            double goldBalance =  double.Parse(Console.ReadLine());
+            Console.WriteLine(goldBalance);
+
+            if (goldBalance >= 400)
+            {
+                Console.WriteLine("Congrats. You have enough to buy a ticket for you and a friend.");
+            }
+            else if (goldBalance >= 200)
+            {
+                Console.WriteLine("Congrats. You have enough to buy at least one ticket.");
+            }
+            else
+            {
+                Console.WriteLine("Too bad. Go make more money.");
+            }
+        }
+    }
+}
